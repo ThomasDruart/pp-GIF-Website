b2a014a4ffd4d729c8b5a1f4592fe6f2e9dfa156
Thomas Druart <thomas.druart.59@gmail.com>
2020-11-24 12:12:03 +0100

    header and footer created

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,13 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function Header() {
-  return <div>Footer</div>;
+const FooterStyled = styled.div`
+  height: 10vh;
+  background-color: #273266;
+  color: white;
+  font-weight: bold;
+`;
+
+export default function Footer() {
+  return <FooterStyled>Footer</FooterStyled>;
 }
