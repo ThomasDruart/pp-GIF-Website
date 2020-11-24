c9d92c2c7c25ecc26ff89bb0b979056f668ffacc
Thomas Druart <thomas.druart.59@gmail.com>
2020-11-24 12:09:57 +0100

    react app structure and import styled components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderStyled = styled.div`
+  height: 10vh;
+  background-color: #273266;
+  color: white;
+  font-weight: bold;
+`;
+
+export default function Header() {
+  return <HeaderStyled>Header</HeaderStyled>;
+}
