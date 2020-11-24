a0ff3dde8af31d947ef6b4de59cea6379a03ce93
Thomas Druart <thomas.druart.59@gmail.com>
2020-11-24 22:20:41 +0100

    Axios working for trending gifs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
 import Header from "./components/Header";
-import Main from "./components/Main";
+import GifList from "./components/GifList";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <Header />
-      <Main />
+      <GifList />
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+/*
+
+TO DO : 
+Fix Each child key // Switch to axios async
+
+Composant : no result found + img/gif
+Composants : gif / gif list (map) / no gif
+Catch Error with popup (react tostify)
+npm i styled reset + balise reset dans index.js avant App
+*/
---
+++ src/components/GifList.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+const SDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export default function GifList() {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    fetchGif();
+  }, []);
+
+  const fetchGif = () => {
+    axios
+      .get("https://api.giphy.com/v1/gifs/trending", {
+        params: {
+          api_key: "Khl0CRBp6qvnFcYuUjNsbk9aTw45Iv8r",
+        },
+      })
+      .then(({ data }) => {
+        setGifs(data.data);
+      });
+  };
+
+  const renderGif = () => {
+    return gifs.map((gif) => {
+      return (
+        <div>
+          <img src={gif.images.fixed_height.url} alt="" />
+        </div>
+      );
+    });
+  };
+  //console.log(gifs);
+
+  return <SDiv>{renderGif()}</SDiv>;
+}
