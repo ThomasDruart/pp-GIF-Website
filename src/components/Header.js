226700cf97d49b074c873f774987202250c62982
Thomas Druart <thomas.druart.59@gmail.com>
2020-11-25 12:00:40 +0100

    header changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,21 +4,39 @@ import styled from "styled-components";
 const SHeader = styled.div`
   height: 10vh;
   background-color: #222b55;
+  display: flex;
+  justify-content: space-around;
 `;
 
 const SH1 = styled.h1`
   color: white;
-  font-weight: bold;
   text-align: center;
   font-size: 1.3em;
   font-family: "Press Start 2P";
-  padding-top: 1em;
+  margin: auto 0;
+`;
+
+const SMenu = styled.ul`
+  font-family: "Press Start 2P";
+  color: white;
+  display: flex;
+  flex-direction: row;
+  margin: auto 0;
+`;
+
+const SLi = styled.li`
+  margin: 0 1.5em;
 `;
 
 export default function Header() {
   return (
     <SHeader>
-      <SH1>Have fun with GIFs</SH1>
+      <SH1>GIF Your Life</SH1>
+      <SMenu>
+        <SLi>Main</SLi>
+        <SLi>Trending</SLi>
+        <SLi>Gif Roulette</SLi>
+      </SMenu>
     </SHeader>
   );
 }
